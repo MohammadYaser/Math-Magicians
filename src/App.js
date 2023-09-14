3f30353d0d19541ba3acb2274c23d829a88dfe7d
MohammadYaser <yasersafi19@outlook.com>
2023-09-14 22:35:30 +0430

    Implement the project desing in Calculator.js file

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import './App.css';
-import { v4 as uuidv4 } from 'uuid';
 import Calculator from './components/Calculator';
 import Screen from './components/Screen';
-import ButtonBox from './components/ButtonBox';
-import Button from './components/Button';
-
-const btnValues = [
-  ['C', '+/-', '%', '/'],
-  [7, 8, 9, 'X'],
-  [4, 5, 6, '-'],
-  [1, 2, 3, '+'],
-  [0, '.', '='],
-];
 
 function App() {
   return (
     <div className="App">
       <Calculator>
         <Screen />
-        <ButtonBox>
-          {btnValues.map((row) => (
-            row.map((btn) => (
-              <Button
-                value={btn}
-                key={uuidv4()}
-              />
-            ))
-          ))}
-        </ButtonBox>
       </Calculator>
     </div>
   );
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { v4 as uuidv4 } from 'uuid';
+import ButtonBox from './ButtonBox';
+import Button from './Button';
+
+const btnValues = [
+  ['C', '+/-', '%', '/'],
+  [7, 8, 9, 'X'],
+  [4, 5, 6, '-'],
+  [1, 2, 3, '+'],
+  [0, '.', '='],
+];
 
 const Calculator = ({ children }) => (
-  <div className="Wrapper">{children}</div>
+  <div className="Wrapper">
+    {children}
+    <ButtonBox>
+      {btnValues.map((row) => (
+        row.map((btn) => (
+          <Button
+            value={btn}
+            key={uuidv4()}
+          />
+        ))
+      ))}
+    </ButtonBox>
+  </div>
 );
 
 Calculator.propTypes = {
